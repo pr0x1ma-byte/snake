60d7662eaf6a7e70882281a569f80f03bac5ad55
Grant <gsowards100@gmail.com>
2017-05-08 22:47:33 -0600

    Removed user class

--- FruitGenerator.py
+++ FruitGenerator.py
@@ -27,32 +27,4 @@ class fruitGenerator(threading.Thread):
       def draw(self):
 	  for i in self.observer.fruitBasket:
 	      unicorn.set_pixel(i[0],i[1],i[2][0],i[2][1],i[2][2]);		
-class user(threading.Thread):
-      def __init__(self,observer):
-          threading.Thread.__init__(self)
-          self.observer = observer
-      def getObserver(self):
-          return self.observer
-      def run(self):
-          ob = self.getObserver()
-          tty.setraw(sys.stdin.fileno())
-          while not ob.exit:
-#                if  bal.getExit():
-#                    bal.setUserExit();
-#                    break;
-#                else:
-                 ch = sys.stdin.read(1)
-                 if (ch == 'w'):
-                    ob.up()
-                 elif (ch == 's'):
-                    ob.down()
-                 elif ch == 'a':
-                    ob.left()
-                 elif ch == 'd':
-                    ob.right()
-                 elif ch == 'b':
-		    ob.end();	
-                    break;
-	  print("size: "+str(ob.snake.size));
-	  ob.end();
-          ob.cleanup(); 	
+	
